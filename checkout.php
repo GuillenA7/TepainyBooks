5297a5292d875dbb4938b8d8bd25a919a03cbe80
GuillenA7 <a22310361@ceti.mx>
2024-05-03 22:40:07 -0600

    Mostrar productos del carrito

--- checkout.php
+++ checkout.php
@@ -1,48 +1,142 @@
+<?php
+
+/**
+ * Pantalla para mostrar el listado de productos en el carrito
+ * Adrian Guillen
+ * 22310361
+ */
+
+require 'config/config.php';
+require 'config/database.php';
+
+$productos = isset($_SESSION['carrito']['productos']) ? $_SESSION['carrito']['productos'] : null;
+
+$db = new Database();
+$con = $db->conectar();
+
+$lista_carrito = array();
+
+if ($productos != null) {
+    foreach ($productos as $clave => $producto) {
+        $sql = $con->prepare("SELECT id, nombre, precio, descuento, $producto AS cantidad FROM productos WHERE id=? AND activo=1");
+        $sql->execute([$clave]);
+        $lista_carrito[] = $sql->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es" class="h-100">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TepainyBooks</title>
 
-    <script src="https://www.paypal.com/sdk/js?client-id=AVY1j-RdBnlFoF2LnPT4gvkGu1-FyOtDVp-SgyzARzrUe8atAoOztdVPSOR-s0DXBTl8sUwjhd1ac3fA&currency=MXN"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="css/estilos.css" rel="stylesheet">
 </head>
 
 <body>
+    <!--Barra de navegación-->
+    <header>
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+            <a href="#" class="navbar-brand">
+                <strong>TepainyBooks</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarHeader">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Catalogo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Soporte</a>
+                    </li>
+                </ul>
+                <a href="carrito.php" class="btn btn-primary">
+                    Carrito<span id="num_cart" class="badge bg-secondary"><?php echo $num_cart; ?></span>
+                </a>
+            </div>
 
-    <div id="paypal-button-container"></div>
+            </div>
+        </div>
+    </header>
 
-    <script>
-        paypal.Buttons({
-            style:{
-                color: 'blue',
-                shape: 'pill',
-                label: 'pay'
-            },
-            createOrder: function(data, actions){
-                return actions.order.create({
-                    purchase_units: [{
-                        amount: {
-                            value: 100
-                        }
-                    }]
-                });
-            },
+    <!--Contenido-->
+    <main>
+        <div class="container">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Precio Unitario</th>
+                            <th>Subtotal</th>
+                            <th>Cantidad</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if ($lista_carrito == null) {
+                            echo '<tr><td colspan="5" class="text-center"><b>Lista vacia</b></td></tr>';
+                        } else {
+                            
+                            $total = 0;
+                            foreach ($lista_carrito as $producto) {
+                                $_id = $producto['id'];
+                                $nombre = $producto['nombre'];
+                                $precio = $producto['precio'];
+                                $descuento = $producto['descuento'];
+                                $cantidad = $producto['cantidad'];
+                                $precio_desc = $precio - (($precio * $descuento) / 100);
+                                $subtotal = $cantidad * $precio_desc;
+                                $total += $subtotal;
+                        ?>
 
-            onApprove: function(data, actions){
-                actions.order.capture().then(function(detalles){
-                    window.location.href="completado.html"
-                });
-            },
+                                <tr>
+                                    <td><?php echo $nombre; ?></td>
+                                    <td><?php echo MONEDA . number_format($precio_desc, 2, '.', ','); ?></td>
+                                    <td>
+                                        <div id="subtotal_<?php echo $_id; ?>" name="subtotal[]"><?php echo MONEDA . number_format($subtotal, 2, '.', ','); ?></div>
+                                    </td>
+                                    <td>
+                                        <input type="number" min="1" max="10" step="1" value="<?php echo $cantidad ?>" size="5" id="cantidad_<?php echo $_id; ?>" onchange="actualizaCantidad(this.value, <?php echo $_id; ?>)">
+                                    </td>
+                                    <td><a href="#" id="eliminar" class="btn btn-warning btn-sm" data-bs-id="<?php echo $_id; ?>" data-bs-toggle="modal" data-bs-target="#eliminaModal">Eliminar</a> </td>
+                                </tr>
+                            <?php } ?>
 
-            onCancel: function(data) {
-                alert("Pago cancelado");
-                console.log(data);
-            }
-        }).render('#paypal-button-container');
-    </script>
+                            <tr>
+                                <td colspan="3"></td>
+                                <td colspan="2">
+                                    <p class="h3" id="total"><?php echo MONEDA . number_format($total, 2, '.', ','); ?></p>
+                                </td>
+                            </tr>
+
+                    </tbody>
+                <?php } ?>
+                </table>
+            </div>
+            
+            <?php if ($lista_carrito != null) { ?>
+                <div class="row">
+                    <div class="col-md-5 offset-md-7 d-grid gap-2">
+                        <a href="pago.php" class="btn btn-primary btn-lg">Realizar pago</a>
+                    </div>
+                </div>
+            <?php } ?>
+
+        </div>
+    </main>
+
+    <!-- Option 1: Bootstrap Bundle with Pooper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 
