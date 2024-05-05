e5b4bf8920d48521eafe7427f0fc17d8873cddac
GuillenA7 <a22310361@ceti.mx>
2024-05-05 15:45:32 -0600

    Detalles de la compra

--- plantilla.php
+++ plantilla.php
@@ -62,34 +62,7 @@ if (!empty($_POST)) {
 
 <body class="d-flex flex-column h-100">
 
-    <!--Barra de navegación-->
-
-    <header>
-        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a href="#" class="navbar-brand">
-                    <strong>TepainyBooks</strong>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="navbarHeader">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link active">Catalogo</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">Soporte</a>
-                        </li>
-                    </ul>
-                    <a href="checkout.php" class="btn btn-primary">
-                        Carrito<span id="num_cart" class="badge bg-secondary"><?php echo $num_cart; ?></span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </header>
+    <?php include 'menu.php'; ?>
 
     <!-- Contenido -->
     <main class="flex-shrink-0">
