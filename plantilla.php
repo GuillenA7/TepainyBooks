a020f772af443092a40642b970f09ba0125caac1
GuillenA7 <a22310361@ceti.mx>
2024-05-05 15:31:26 -0600

    Historial de compras

--- plantilla.php
+++ plantilla.php
@@ -56,6 +56,7 @@ if (!empty($_POST)) {
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="css/all.min.css" rel="stylesheet">
     <link href="css/estilos.css" rel="stylesheet">
 </head>
 
