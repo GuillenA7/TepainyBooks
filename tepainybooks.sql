5d6fb35bf2ed92a068e17e01fdf978edcf42ca7c
GuillenA7 <a22310361@ceti.mx>
2024-06-18 21:26:00 -0600

    nose

--- tepainybooks.sql
+++ tepainybooks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-06-2024 a las 04:05:39
+-- Tiempo de generación: 19-06-2024 a las 05:20:52
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -49,6 +49,20 @@ INSERT INTO `admin` (`id`, `usuario`, `password`, `nombre`, `email`, `token_pass
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `bitacora`
+--
+
+CREATE TABLE `bitacora` (
+  `id` int(11) NOT NULL,
+  `accion` varchar(50) NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `sentencia` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `categorias`
 --
 
@@ -63,9 +77,9 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `activo`) VALUES
-(1, 'ZAPATOS', 1),
-(2, 'COMPUTACIÓN', 1),
-(3, 'TELEFÓNOS', 1);
+(1, 'Libro de autoayuda', 1),
+(2, 'Literatura infantil', 1),
+(3, 'Espiritualidad', 1);
 
 -- --------------------------------------------------------
 
@@ -115,7 +129,9 @@ CREATE TABLE `compra` (
 --
 
 INSERT INTO `compra` (`id`, `id_transaccion`, `fecha`, `status`, `email`, `id_cliente`, `total`, `medio_pago`) VALUES
-(1, '8S53475734486115A', '2024-06-18 23:22:09', 'COMPLETED', 'adrianguillen2004@gmail.com', '1', 539.10, 'paypal');
+(1, '8S53475734486115A', '2024-06-18 23:22:09', 'COMPLETED', 'adrianguillen2004@gmail.com', '1', 539.10, 'paypal'),
+(2, '10791941EM933804P', '2024-06-19 04:12:08', 'COMPLETED', 'adrianguillen2004@gmail.com', '1', 11400.00, 'paypal'),
+(3, '2AJ26466A1682911Y', '2024-06-19 04:51:20', 'COMPLETED', 'adrianguillen2004@gmail.com', '1', 643.95, 'paypal');
 
 -- --------------------------------------------------------
 
@@ -166,7 +182,11 @@ CREATE TABLE `detalle_compra` (
 --
 
 INSERT INTO `detalle_compra` (`id`, `id_compra`, `id_producto`, `nombre`, `precio`, `cantidad`) VALUES
-(1, 1, 1, 'Zapatos color cafe', 539.10, 1);
+(1, 1, 1, 'Zapatos color cafe', 539.10, 1),
+(2, 2, 2, 'Laptop 15.6\" con Windows 11', 11400.00, 1),
+(3, 3, 4, 'Los Cuatro Acuerdos: Un libro de sabiduría tolteca', 225.25, 1),
+(4, 3, 6, 'Bluey. ¿Dónde está Bluey?', 119.00, 1),
+(5, 3, 5, 'Deja De Ser Tú: La mente crea la realidad', 299.70, 1);
 
 -- --------------------------------------------------------
 
@@ -191,9 +211,12 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `slug`, `nombre`, `descripcion`, `precio`, `descuento`, `stock`, `id_categoria`, `activo`) VALUES
-(1, 'zapatos-color-cafe', 'Zapatos color cafe', '<p>Zapato Confort casual para caballero color café, con corte de piel natural vacuno, acabado flother, diseño de costura en los costados y sutil diseño punteado.</p><p><br><strong>Características:</strong><br>Marca: Cklass<br>Colección: Otoño-Invierno 2021<br>Modelo: 273-29<br>Color: Café<br>Material corte: Piel Natural Vacuno<br>Material Suela: Sintética<br>&nbsp;</p>', 599.00, 10, 999, 1, 1),
-(2, 'laptop-15-6-con-windows-11', 'Laptop 15.6\" con Windows 11', '<p>Capacidad: <strong>12 GB RAM</strong> | <strong>512 GB SSD</strong>&nbsp;<br>Procesador: <strong>Intel Core i7 1165G7</strong>&nbsp;<br>Tarj. gráfica: <strong>Intel Iris Xe Graphics G7 96EUs</strong>&nbsp;<br>&nbsp;</p><p><strong>Más detalles de este producto</strong></p><ul><li>Pantalla LCD de 15.6\".</li><li>Resolución de 1920x1080 px.</li><li>Es antirreflejo.</li><li>Es táctil.</li><li>Conexión wifi y bluetooth.</li><li>Cuenta con 3 puertos USB y puerto HDMI.</li></ul>', 12000.00, 5, 1000, 2, 1),
-(3, 'smartphone-negro-32gb-dual-sim-3gb-ram', 'Smartphone Negro 32gb Dual Sim 3gb Ram', '<p>Galaxy A02 es un equipo que te brinda excelentes beneficios: Su doble cámara trasera de 13 MP +2 MP y una cámara frontal de 5 MP te permitirán capturar los mejores momentos con una gran calidad.</p><p>Memoria interna: <strong>32 GB</strong>&nbsp;<br>Memoria RAM: <strong>3 GB</strong>&nbsp;<br>Cámara frontal principal: <strong>5 Mpx</strong>&nbsp;<br>Cámara trasera principal: <strong>13 Mpx</strong></p>', 2899.00, 0, 1000, 3, 1);
+(1, 'h-bitos-at-micos', 'Hábitos Atómicos', '<p>HÁBITOS ATÓMICOS parte de una simple pero poderosa pregunta: ¿Cómo podemos vivir mejor? Sabemos que unos buenos hábitos nos permiten mejorar significativamente nuestra vida, pero con frecuencia nos desviamos del camino: dejamos de hacer ejercicio, comemos mal, dormimos poco, despilfarramos. ¿Por qué es tan fácil caer en los malos hábitos y tan complicado seguir los buenos? James Clear nos brinda fantásticas ideas basadas en investigaciones científicas, que le permiten revelarnos cómo podemos transformar pequeños hábitos cotidianos para cambiar nuestra vida y mejorarla. Esta guía pone al descubierto las fuerzas ocultas que moldean nuestro comportamiento —desde nuestra mentalidad, pasando por el ambiente y hasta la genética— y nos demuestra cómo aplicar cada cambio a nuestra vida y a nuestro trabajo. Después de leer este libro, tendrás un método sencillo para desarrollar un sistema eficaz que te conducirá al éxito. Aprende cómo… Darte tiempo para desarrollar nuevos hábitos Superar la falta de motivación y de fuerza de voluntad Diseñar un ambiente para que el éxito sea fácil de alcanzar Regresar al buen camino cuando te hayas desviado un poco</p>', 398.00, 0, 999, 1, 0),
+(2, 'el-poder-del-ahora', 'El poder del ahora', '<p>El clásico que consagró a Eckhart Tolle como uno de los gurús más importantes del mundo. El poder del ahora es un libro único. Tiene la capacidad de crear una experiencia en los lectores y de cambiar su vida. Hoy ya es considerado una obra maestra. Su autor, Eckhart Tolle, se convirtió en un maestro universal, un gran espíritu con un gran mensaje: se puede alcanzar un estado de iluminación aquí y ahora. Es posible vivir libre del sufrimiento, de la ansiedad y de la neurosis. Para lograrlo sólo tenemos que comprender nuestro papel de creadores de nuestro dolor. Es nuestra propia mente la que causa nuestros problemas con su corriente constante de pensamientos, aferrándose al pasado, preocupándose por el futuro. Cometemos el error de identificarnos con ella, de pensar que eso es lo que somos, cuando de hecho somos seres mucho más grandes. Escrito en un formato de preguntas y respuestas que lo hace muy accesible, El poder del ahora es una invitación a la reflexión, que le abrirá las puertas a la plenitud espiritual y le permitirá verla vida con nuevos ojos y empezar a disfrutar del verdadero poder del ahora.</p>', 429.00, 59, 999, 1, 0),
+(3, 'el-principito', 'El Principito', '<p>Pocas historias son tan universalmente leídas y apreciadas tanto por niños como adultos por igual como El Principito. Esta obra, que ha capturado los corazones de los lectores de todas las edades, es un cuento poético que viene acompañado de ilustraciones hechas con acuarelas por el mismo Saint-Exupéry. En él, un piloto se encuentra perdido en el desierto del Sahara después de que su avión sufriera una avería, pero para su sorpresa, es ahí donde conoce a un pequeño príncipe proveniente de otro planeta, y que lo enfrentará a muchos mundos que son su propio mundo.<br>A pesar de que es considerado un libro infantil por la forma en la que se encuentra escrito, también posee observaciones profundas sobre la vida y la naturaleza humanas.</p>', 62.00, 0, 1000, 2, 0),
+(4, 'los-cuatro-acuerdos-un-libro-de-sabidur-a-tolteca', 'Los Cuatro Acuerdos: Un libro de sabiduría tolteca', '<p>El conocimiento tolteca surge de la misma unidad esencial de la verdad de la que parten todas las tradiciones esotéricas sagradas del mundo. Aunque no es una religión, respeta a todos los maestros espirituales que han enseñado en la tierra, y si bien abraza el espíritu, resulta más preciso describirlo como una manera de vivir que se distingue por su fácil acceso a la felicidad y el amor. El doctor Miguel Ruiz nos propone en este libro un sencillo procedimiento para eliminar todas aquellas creencias heredadas que nos limitan y substituirlas por otras que responden a nuestra realidad interior y nos conducen a la libertad. Hace miles de años los toltecas eran conocidos en todo el sur de México como «mujeres y hombres de conocimiento». Los antropólogos han definido a los toltecas como una nación o una raza, pero de hecho, eran científicos y artistas que formaron una sociedad para estudiar y conservar el conocimiento espiritual y las prácticas de sus antepasados. La conquista europea, unida a un agresivo abuso del poder personal por parte de algunos aprendices, hizo que los naguales se vieran forzados a esconder su sabiduría ancestral y a mantener su existencia en la oscuridad. Por fortuna, el conocimiento esotérico tolteca fue conservado y transmitido de una generación a otra por distintos linajes de naguales. Ahora, el doctor Miguel Ruiz, un nagual del linaje de los Guerreros del Águila, comparte con nosotros las profundas enseñanzas de los toltecas.</p>', 265.00, 15, 999, 3, 1),
+(5, 'deja-de-ser-t-la-mente-crea-la-realidad', 'Deja De Ser Tú: La mente crea la realidad', '<p><strong>Joe Dispenza</strong> saltó a la fama en nuestro país tras participar en la película ¿Y tú qué sabes?, un documental sobre la sobrecogedora capacidad de la mente para transformar la realidad que corrió de mano en mano sin ninguna publicidad, gracias al boca oreja.</p><p>Ahora, el popularísimo científico y autor de Desarrolla tu cerebro profundiza en todos aquellos temas que tanto nos cautivaron -física cuántica, neurociencia, biología y genética- para enseñarnos a reprogramar el cerebro y ampliar nuestro marco de realidad.</p><p>El resultado es un método práctico de trasformación para crear prosperidad y riqueza, pero también un viaje prodigioso a un nuevo estado de conciencia.</p>', 370.00, 19, 999, 1, 1),
+(6, 'bluey-d-nde-est-bluey', 'Bluey. ¿Dónde está Bluey?', '<p><strong>¿Has visto a BLUEY y a BINGO?</strong></p><p>También hay muchas más cosas ocultas, ¡así que únete a la diversión en este libro de busca y encuentra!</p>', 119.00, 0, 999, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -227,6 +250,12 @@ INSERT INTO `usuarios` (`id`, `usuario`, `password`, `activacion`, `token`, `tok
 -- Indices de la tabla `admin`
 --
 ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `bitacora`
+--
+ALTER TABLE `bitacora`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -283,6 +312,12 @@ ALTER TABLE `admin`
   MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT de la tabla `bitacora`
+--
+ALTER TABLE `bitacora`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
@@ -298,7 +333,7 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de la tabla `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `configuracion`
@@ -310,13 +345,13 @@ ALTER TABLE `configuracion`
 -- AUTO_INCREMENT de la tabla `detalle_compra`
 --
 ALTER TABLE `detalle_compra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
